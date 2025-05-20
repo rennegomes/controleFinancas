5b0ad62acc69834e03cbc9722e9cebb69e3195df
rennegomes <renne.lace082@gmail.com>
2025-05-20 18:13:34 -0300

    :sparkles: feat: Componente Resumo

---
+++ src/app/components/resumo/resumo.tsx
@@ -0,0 +1,36 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "@phosphor-icons/react/dist/ssr";
+
+export default function Resumo() {
+  return (
+   <div className="flex justify-between overflow-x-auto whitespace-nowrap md:grid grid-cols-3 gap-8 mt-[-5rem] w-full max-w-6xl mx-auto px-6 pb-2">
+
+    <div className="flex flex-col min-w-72 bg-[var(--cor-destaque)] p-5 rounded-md">
+        <div className="flex justify-between items-center pb-5">
+            <span>Entradas</span>
+            <ArrowCircleUp size={32} className="text-[var(--cor-positiva-verde)]" />
+        </div>
+        <strong className="text-[var(--cor-texto-destaque)] text-2xl">R$ 15.500,00</strong>
+        <span className="text-[var(--cor-texto-sem-destaque)] text-sm">Última entrada em 13 de abril</span>
+    </div>
+
+    <div className="flex flex-col min-w-72 bg-[var(--cor-destaque)] p-5 rounded-md">
+        <div className="flex justify-between items-center pb-5">
+            <span>Saídas</span>
+            <ArrowCircleDown size={32} className="text-[var(--cor-negativa-vermelho)]" />
+        </div>
+        <strong className="text-[var(--cor-texto-destaque)] text-2xl">R$ 15.500,00</strong>
+        <span className="text-[var(--cor-texto-sem-destaque)] text-sm">Última entrada em 13 de abril</span>
+    </div>
+
+    <div className="flex flex-col min-w-72 bg-[var(--cor-positiva-verde-escuro)] p-5 rounded-md">
+        <div className="flex justify-between items-center pb-5">
+            <span>Total</span>
+            <CurrencyDollar size={32} className="text-[var(--cor-texto-destaque)]" />
+        </div>
+        <strong className="text-[var(--cor-texto-destaque)] text-2xl">R$ 15.500,00</strong>
+        <span className="text-sm">Última entrada em 13 de abril</span>
+    </div>
+
+   </div>
+  );
+}
--- src/app/pages/inicio/inicio.tsx
+++ src/app/pages/inicio/inicio.tsx
@@ -1,9 +1,11 @@
 import Cabacalho from "@/app/components/cabecalho/cabecalho";
+import Resumo from "@/app/components/resumo/resumo";
 
 export default function Inicio() {
   return (
    <div>
       <Cabacalho />
+      <Resumo />
    </div>
   );
 }
