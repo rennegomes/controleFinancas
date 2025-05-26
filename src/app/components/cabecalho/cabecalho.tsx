72944200ba558557f411571566456a48b14a1e5d
rennegomes <renne.lace082@gmail.com>
2025-05-26 16:38:19 -0300

    :sparkles: feat: Componente ConteudoDialog

--- src/app/components/cabecalho/cabecalho.tsx
+++ src/app/components/cabecalho/cabecalho.tsx
@@ -1,11 +1,21 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import ConteudoDialog from "../conteudoDialog/conteudoDialog";
+
 export default function Cabacalho() {
   return (
    <div className="bg-[var(--background)] pt-10 pb-28">
     <div className="flex justify-between items-center w-full max-w-6xl mx-auto px-6">
       <div></div>
-      <button className="bg-[var(--cor-positiva-verde-escuro)] text-[var(--cor-texto-destaque)] font-bold h-12 px-5 rounded-sm cursor-pointer hover:bg-[var(--cor-positiva-verde)] transition-all duration-200">
-        Nova transação
-      </button>
+
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <button className="bg-[var(--cor-positiva-verde-escuro)] text-[var(--cor-texto-destaque)] font-bold h-12 px-5 rounded-sm cursor-pointer hover:bg-[var(--cor-positiva-verde)] transition-all duration-200">
+            Nova transação
+          </button>
+        </Dialog.Trigger>
+        <ConteudoDialog />
+      </Dialog.Root>
+
     </div>
    </div>
   );
---
+++ src/app/components/conteudoDialog/conteudoDialog.tsx
@@ -0,0 +1,50 @@
+import { X } from "@phosphor-icons/react/dist/ssr";
+import * as Dialog from "@radix-ui/react-dialog";
+
+export default function ConteudoDialog(){
+    return(
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 w-screen h-screen bg-[var(--background)]/50" />
+
+          <Dialog.Content className="fixed p-4 bg-[var(--cor-destaque)] rounded-t-3xl pt-5 left-0 right-0 max-sm:bottom-0 md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:rounded-md md:max-h-[85vh] md:w-[90vw] md:max-w-[500px]">
+            <div className="flex justify-between">
+              <Dialog.Title className="text-[17px] font-medium">Nova Transação</Dialog.Title>
+
+              <Dialog.Close asChild>
+                <button className="font-bold cursor-pointer">
+                  <X size={20} />
+                </button>
+              </Dialog.Close>
+            </div>
+            <form className="flex flex-col gap-1 mt-4">
+              <input 
+                type="text" 
+                name="" 
+                id="" 
+                placeholder="Descrição" 
+                required 
+                className="bg-[var(--background)] w-full rounded-md p-4 focus:outline-none"
+              />
+              <input 
+                type="number"
+                name="" 
+                id="" 
+                placeholder="Preço" 
+                required 
+                className="bg-[var(--background)] w-full rounded-md p-4 focus:outline-none"
+              />
+              <input 
+                type="text" 
+                name="" 
+                id="" 
+                placeholder="Categoria" 
+                required 
+                className="bg-[var(--background)] w-full rounded-md p-4 focus:outline-none"
+              />
+
+              <input type="submit" value="Cadastrar" className="bg-[var(--cor-positiva-verde-escuro)] p-2 my-5 rounded-md" />
+            </form>
+          </Dialog.Content>
+        </Dialog.Portal>
+    )
+}
