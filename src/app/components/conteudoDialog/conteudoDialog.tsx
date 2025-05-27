d8e1ed5d62f9edc0df4c01b3bdb852dd74884d20
rennegomes <renne.lace082@gmail.com>
2025-05-27 17:01:12 -0300

    :construction: Botões de entrada de saída do componente conteudoDialog

--- src/app/components/conteudoDialog/conteudoDialog.tsx
+++ src/app/components/conteudoDialog/conteudoDialog.tsx
@@ -1,4 +1,4 @@
-import { X } from "@phosphor-icons/react/dist/ssr";
+import { ArrowCircleDownIcon, ArrowCircleUpIcon, X } from "@phosphor-icons/react/dist/ssr";
 import * as Dialog from "@radix-ui/react-dialog";
 
 export default function ConteudoDialog(){
@@ -42,7 +42,22 @@ export default function ConteudoDialog(){
                 className="bg-[var(--background)] w-full rounded-md p-4 focus:outline-none"
               />
 
-              <input type="submit" value="Cadastrar" className="bg-[var(--cor-positiva-verde-escuro)] p-2 my-5 rounded-md" />
+              <div className="grid grid-cols-2 gap-5">
+                <button className="flex gap-2 justify-center bg-[var(--cor-off-destaque)] rounded-md p-4 mt-5 cursor-pointer text-[var(--cor-positiva-verde)] focus:text-[var(--cor-texto-padrao)] focus:bg-[var(--cor-positiva-verde-escuro)]">
+                  <ArrowCircleUpIcon size={24} />
+                  <span className="text-[var(--cor-texto-padrao)]">Entrada</span>
+                </button>
+                <button className="flex gap-2 justify-center bg-[var(--cor-off-destaque)] rounded-md p-4 mt-5 cursor-pointer text-[var(--cor-negativa-vermelho)] focus:text-[var(--cor-texto-padrao)] focus:bg-[var(--cor-negativa-vermelho-escuro)]">
+                  <ArrowCircleDownIcon size={24} />
+                  <span className="text-[var(--cor-texto-padrao)]">Saída</span> 
+                </button>
+              </div>
+
+              <input 
+                type="submit" 
+                value="Cadastrar" 
+                className="bg-[var(--cor-positiva-verde-escuro)] p-2 my-5 rounded-md font-bold cursor-pointer" 
+              />
             </form>
           </Dialog.Content>
         </Dialog.Portal>
