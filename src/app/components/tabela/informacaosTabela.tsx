3249000e56f88e73311572232cc81b4437afef6a
rennegomes <renne.lace082@gmail.com>
2025-07-01 16:26:33 -0300

    :package: build: Otimizando jsonServer

--- src/app/components/tabela/informacaosTabela.tsx
+++ src/app/components/tabela/informacaosTabela.tsx
@@ -1,26 +1,31 @@
 interface InformacoesTabelaProps {
-    corEntradaESaida: "entrada" | "saida"
+    id: number
+    descricao: string
+    tipo: 'entrada' | 'saida'
+    categoria: string
+    preco: number
+    dataCriacao: string
 }
 
 export default function InfromacoesTabela(props: InformacoesTabelaProps){
     return(
         <tbody>
             <tr className="max-sm:hidden">
-                <td className="bg-[var(--cor-destaque)] w-1/2 first:rounded-l-md last:rounded-r-md py-5 px-8">Desenvolvimento de site</td>
-                <td className={props.corEntradaESaida === "saida" ? "text-[var(--cor-negativa-vermelho)] bg-[var(--cor-destaque)] w-1/3 first:rounded-l-md last:rounded-r-md py-5 px-8" : "text-[var(--cor-positiva-verde)] bg-[var(--cor-destaque)] w-1/3 first:rounded-l-md last:rounded-r-md py-5 px-8"}>R$ 12.000,00</td>
-                <td className="bg-[var(--cor-destaque)] first:rounded-l-md last:rounded-r-md py-5 px-8">Venda</td>
-                <td className="bg-[var(--cor-destaque)] first:rounded-l-md last:rounded-r-md py-5 px-8">13/04/2022</td>
+                <td className="bg-[var(--cor-destaque)] w-1/2 first:rounded-l-md last:rounded-r-md py-5 px-8">{props.descricao}</td>
+                <td className={props.tipo === "saida" ? "text-[var(--cor-negativa-vermelho)] bg-[var(--cor-destaque)] w-1/3 first:rounded-l-md last:rounded-r-md py-5 px-8" : "text-[var(--cor-positiva-verde)] bg-[var(--cor-destaque)] w-1/3 first:rounded-l-md last:rounded-r-md py-5 px-8"}>R$ {props.preco}</td>
+                <td className="bg-[var(--cor-destaque)] first:rounded-l-md last:rounded-r-md py-5 px-8">{props.categoria}</td>
+                <td className="bg-[var(--cor-destaque)] first:rounded-l-md last:rounded-r-md py-5 px-8">{props.dataCriacao}</td>
             </tr>
 
             <tr className="sm:hidden">
                 <td className="bg-[var(--cor-destaque)] first:rounded-l-md last:rounded-r-md py-5 px-8">
                     <div className="flex flex-col gap-2">
-                        <span>Desenvolvimento de site</span>
-                        <span className={props.corEntradaESaida === "saida" ? "text-[var(--cor-negativa-vermelho)] font-bold text-xl" : "text-[var(--cor-positiva-verde)] font-bold text-xl"}>R$ 12.000,00</span>
+                        <span>{props.descricao}</span>
+                        <span className={props.tipo === "saida" ? "text-[var(--cor-negativa-vermelho)] font-bold text-xl" : "text-[var(--cor-positiva-verde)] font-bold text-xl"}>R$ {props.preco}</span>
                     </div>
                     <div className="flex justify-between pt-4 text-sm text-[var(--cor-texto-sem-destaque)]">
-                        <span>Venda</span>
-                        <samp>13/04/2022</samp>
+                        <span>{props.categoria}</span>
+                        <samp>{props.dataCriacao}</samp>
                     </div>
                 </td>
             </tr>
--- src/app/components/tabela/tabela.tsx
+++ src/app/components/tabela/tabela.tsx
@@ -1,11 +1,40 @@
+'use client'
+import { useEffect, useState } from "react";
 import InfromacoesTabela from "./informacaosTabela";
 
+interface Transacoes{
+    id: number
+    descricao: string
+    tipo: 'entrada' | 'saida'
+    categoria: string
+    preco: number
+    dataCriacao: string
+}
+
 export default function Tabela(){
+    const [transacoes, setTransacoes] = useState<Transacoes[]>([]);
+
+    async function carregaTransacao(){
+        const response = await fetch('http://localhost:3333/trasacao')
+        const data = await response.json();
+
+        setTransacoes(data);
+    }
+
+    useEffect(() => {
+        carregaTransacao();
+    }, [])
+
+
     return(
         <div className="flex justify-between items-center w-full max-w-6xl mx-auto px-6 mt-6">
             <table className="w-full border-separate border-spacing-y-2">
-                <InfromacoesTabela corEntradaESaida="entrada"/>
-                <InfromacoesTabela corEntradaESaida="saida"/>
+                {transacoes.map(transacoes => {
+                    return(
+                        <InfromacoesTabela key={transacoes.id} id={transacoes.id} descricao={transacoes.descricao} tipo={transacoes.tipo} categoria={transacoes.categoria} preco={transacoes.preco} dataCriacao={transacoes.dataCriacao} />
+                    )
+                })}
+                
             </table>
         </div>
     )
